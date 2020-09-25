141a388b669211799dbb7918dc228cf4a34e401f
Anton Zlygostev <zlygostev@hotmail.com>
2020-09-25 12:06:00 +0700

    Removed false dependency, made Result static.

--- Linq2d/Result.cs
+++ Linq2d/Result.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Linq2d
+﻿namespace Linq2d
 {
-    public class Result<R>:Result
+    public class Result<R> 
     {
         public R InitValue { get; }
         public Result(R initValue) => InitValue = initValue;
     }
 
-    public class Result
+    public static class Result
     {
         public static Result<R> SubstBy<R>(R substValue) => new Result<R>(substValue);
     }
