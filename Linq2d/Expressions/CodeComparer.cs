f2c2192ba0494b91f740571af26cda0437c61a57
Anton Zlygostev <zlygostev@hotmail.com>
2020-09-25 12:12:31 +0700

    Now the comparer respects the values of the call arguments

--- Linq2d/Expressions/CodeComparer.cs
+++ Linq2d/Expressions/CodeComparer.cs
@@ -31,7 +31,8 @@ namespace Linq2d.Expressions
                 (ConstantExpression c1, ConstantExpression c2) => Equals(c1.Value, c2.Value),
                 (UnaryExpression u1, UnaryExpression u2) => Equals(u1.Operand, u2.Operand),
                 (BinaryExpression b1, BinaryExpression b2) => Equals(b1.Left, b2.Left) && Equals(b1.Right, b2.Right),
-                (MethodCallExpression m1, MethodCallExpression m2) => Equals(m1.Object, m2.Object) && m1.Method == m2.Method,
+                (MethodCallExpression m1, MethodCallExpression m2) 
+                    => Equals(m1.Object, m2.Object) && m1.Method == m2.Method && Enumerable.SequenceEqual(m1.Arguments, m2.Arguments, this),
                 (IndexExpression i1, IndexExpression i2) => Equals(i1.Object, i2.Object) && Enumerable.SequenceEqual(i1.Arguments, i2.Arguments, this),
                 (MemberExpression m1, MemberExpression m2) => Equals(m1.Expression, m2.Expression) && m1.Member == m2.Member,
                 (BlockExpression b1, BlockExpression b2) => Enumerable.SequenceEqual(b1.Expressions, b2.Expressions, this) && Enumerable.SequenceEqual(b1.Variables, b2.Variables),
