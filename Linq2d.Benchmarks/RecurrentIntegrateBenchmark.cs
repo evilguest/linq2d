acd4dc430a454e72ced35cf9ec99674e20b0938b
Anton Zlygostev <zlygostev@hotmail.com>
2020-09-30 16:39:58 +0700

    Fixed the code for non-AVX-enabled environments

--- Linq2d.Benchmarks/RecurrentIntegrateBenchmark.cs
+++ Linq2d.Benchmarks/RecurrentIntegrateBenchmark.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using ImageHelpers;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
 using Mono.Linq.Expressions;
 using System;
 using System.Runtime.CompilerServices;
@@ -17,16 +18,15 @@ namespace Linq2d.Benchmarks
             base.Initialize();
             Array2d.TryVectorize = true;
             var vectorIntegrate = GetIntegrate();
+            var _ = vectorIntegrate.Transform; // force compilation
             IVectorizable ev = ((IVectorizable)vectorIntegrate);
             if (!ev.Vectorized)
             {
                 Console.Error.Write("Recurrent integration failed");
-                if (ev.VectorizationResult != null)
-                    Console.Error.WriteLine($" due to the expression\n{ev.VectorizationResult.BlockedBy.ToCSharpCode()}:\n  {ev.VectorizationResult.Reason}");
-                else
-                    Console.Error.WriteLine();
+                Console.Error.WriteLine($" due to the expression\n{ev.VectorizationResult.BlockedBy.ToCSharpCode()}:\n  {ev.VectorizationResult.Reason}");
             }
-            _integrateVector = vectorIntegrate.Transform;
+            else 
+              _integrateVector = vectorIntegrate.Transform;
 
             Array2d.TryVectorize = false;
             _integrateScalar = GetIntegrate().Transform;
@@ -35,18 +35,17 @@ namespace Linq2d.Benchmarks
         private IArrayTransform<byte, int> GetIntegrate()
         {
             return (from d in _data
-                    from r in Result.SubstBy(0)
+                    from r in Result.InitWith(0)
                     select d + r[0, -1] + r[-1, 0] - r[-1, -1]);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Vector256<int> Aggregate(Vector256<int> t, int carry)
         {
-            var shiftRight = RightShift;
+            var shiftRight = RotateRight;
             var t2 = Avx2.PermuteVar8x32(t, shiftRight); t2 = t2.WithElement(0, carry);
             t = Avx2.Add(t, t2);
-            t2 = Avx2.PermuteVar8x32(t2, shiftRight); t2 = t2.WithElement(0, 0);
-            t = Avx2.Add(t, t2);
+            t2 = Avx2.PermuteVar8x32(t2, shiftRight); t2 = t2.WithElement(0, 0);  t = Avx2.Add(t, t2);
             t2 = Avx2.PermuteVar8x32(t2, shiftRight); t = Avx2.Add(t, t2);
             t2 = Avx2.PermuteVar8x32(t2, shiftRight); t = Avx2.Add(t, t2);
             t2 = Avx2.PermuteVar8x32(t2, shiftRight); t = Avx2.Add(t, t2);
@@ -59,7 +58,7 @@ namespace Linq2d.Benchmarks
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Vector256<int> Aggregate(Vector256<int> prev, Vector256<int> curr)
         {
-            var shiftRight = RightShift;
+            var shiftRight = RotateRight;
             Vector256<int> temp;
             temp = Avx2.PermuteVar8x32(curr, shiftRight); temp = temp.WithElement(0, prev.GetElement(7)); var r = temp;
             temp = Avx2.PermuteVar8x32(temp, shiftRight); temp = temp.WithElement(0, prev.GetElement(6)); r = Avx2.Add(r, temp);
@@ -156,7 +155,7 @@ namespace Linq2d.Benchmarks
             int h = _data.Height();
             int[,] res = new int[h, w];
 
-            Vector256<int> shiftRight = RightShift;
+            Vector256<int> shiftRight = RotateRight;
 
             fixed (byte* pSource = &_data[0, 0])
             fixed (int* pTarget = &res[0, 0])
@@ -236,9 +235,19 @@ namespace Linq2d.Benchmarks
         [Benchmark]
         public int[,] IntegrateLinqScalar()
         {
+            Array2d.TryVectorize = false;
             return GetIntegrate().ToArray();
         }
 
+        [Benchmark]
+        public int[,] IntegrateLinqVector()
+        {
+            Array2d.TryVectorize = true;
+            var q = GetIntegrate();
+            if (q is IVectorizable v && !v.Vectorized)
+                throw new InvalidOperationException($"Vectorization failed due to {v.VectorizationResult.Reason}");
+            return q.ToArray();
+        }
 
         /* private unsafe static void VerticalIntegrate(byte[,] source, int[,] target, int startColumn, int width)
         {
@@ -325,86 +334,86 @@ namespace Linq2d.Benchmarks
         }
     */
 
-    /*
-    /// <summary>
-    /// Divides w into numTasks bins so:
-    /// - Each bin size (except, maybe, the last one) is divisible by basis
-    /// - The bins are relatively to each other in size
-    /// </summary>
-    /// <param name="w"></param>
-    /// <param name="num"></param>
-    /// <param name="basis"></param>
-    /// <returns></returns>
-    private static int[] DivideEvenly(int w, int num, int basis)
-    {
-        var n = w / basis;
-        var r1 = w % basis;
-        var k = n / num;
-        var r2 = n % num;
-
-        var res = new int[num];
-
-        int i = 0;
-        while (i < r2)
-            res[i++] = (k + 1) * basis;
-
-        res[i++] = k * basis + r1;
-
-        while (i < num)
-            res[i++] = k * basis;
-        return res;
-    }
-
-    [Benchmark] // 2 place; +25% to the winner
-    unsafe public int[,] IntegrateAvx2Pass()
-    {
-
-        // we will do two passes, as it is multicore-friendlier than having a single-pass.
-
-        int w = _data.Width();
-        int h = _data.Height();
-
-        // pass 1: top-to-bottom aggregation
-
-        int[,] step1 = new int[h, w];
-
-        var numTasks = Math.Min(w / Vector256<int>.Count, System.Environment.ProcessorCount);
-
-        var taskWidths = DivideEvenly(w, numTasks, Vector256<int>.Count);
-        var vTasks = new Task[numTasks];
-        var position = 0;
-        for (var i = 0; i < numTasks; i++)
+        /*
+        /// <summary>
+        /// Divides w into numTasks bins so:
+        /// - Each bin size (except, maybe, the last one) is divisible by basis
+        /// - The bins are relatively to each other in size
+        /// </summary>
+        /// <param name="w"></param>
+        /// <param name="num"></param>
+        /// <param name="basis"></param>
+        /// <returns></returns>
+        private static int[] DivideEvenly(int w, int num, int basis)
         {
-            var p = position; var width = taskWidths[i];
-            vTasks[i] = Task.Run(() => VerticalIntegrate(_data, step1, p, width));
-            position += taskWidths[i];
+            var n = w / basis;
+            var r1 = w % basis;
+            var k = n / num;
+            var r2 = n % num;
+
+            var res = new int[num];
+
+            int i = 0;
+            while (i < r2)
+                res[i++] = (k + 1) * basis;
+
+            res[i++] = k * basis + r1;
+
+            while (i < num)
+                res[i++] = k * basis;
+            return res;
         }
 
-        Task.WaitAll(vTasks);
-
-        // Now in step1 we have the vertical-integrated array; so now it is time to perform the horizontal integration. 
-
-        var step2 = new int[h, w];
-
-        numTasks = Math.Min(h / Vector256<int>.Count, System.Environment.ProcessorCount);
-
-        var taskHeights = DivideEvenly(h, numTasks, Vector256<int>.Count);
-        var hTasks = new Task[numTasks];
-        position = 0;
-        for (var i = 0; i < numTasks; i++)
+        [Benchmark] // 2 place; +25% to the winner
+        unsafe public int[,] IntegrateAvx2Pass()
         {
-            var p = position; var height = taskHeights[i];
-            hTasks[i] = Task.Run(() => HorizontalIntegrate(step1, step2, p, height));
-            position += taskHeights[i];
+
+            // we will do two passes, as it is multicore-friendlier than having a single-pass.
+
+            int w = _data.Width();
+            int h = _data.Height();
+
+            // pass 1: top-to-bottom aggregation
+
+            int[,] step1 = new int[h, w];
+
+            var numTasks = Math.Min(w / Vector256<int>.Count, System.Environment.ProcessorCount);
+
+            var taskWidths = DivideEvenly(w, numTasks, Vector256<int>.Count);
+            var vTasks = new Task[numTasks];
+            var position = 0;
+            for (var i = 0; i < numTasks; i++)
+            {
+                var p = position; var width = taskWidths[i];
+                vTasks[i] = Task.Run(() => VerticalIntegrate(_data, step1, p, width));
+                position += taskWidths[i];
+            }
+
+            Task.WaitAll(vTasks);
+
+            // Now in step1 we have the vertical-integrated array; so now it is time to perform the horizontal integration. 
+
+            var step2 = new int[h, w];
+
+            numTasks = Math.Min(h / Vector256<int>.Count, System.Environment.ProcessorCount);
+
+            var taskHeights = DivideEvenly(h, numTasks, Vector256<int>.Count);
+            var hTasks = new Task[numTasks];
+            position = 0;
+            for (var i = 0; i < numTasks; i++)
+            {
+                var p = position; var height = taskHeights[i];
+                hTasks[i] = Task.Run(() => HorizontalIntegrate(step1, step2, p, height));
+                position += taskHeights[i];
+            }
+
+            Task.WaitAll(hTasks);
+
+            return step2;
         }
+        */
 
-        Task.WaitAll(hTasks);
-
-        return step2;
-    }
-    */
-
-        private static readonly unsafe Vector256<int> RightShift = Vector256.Create(0, 0, 1, 2, 3, 4, 5, 6);
+        private static readonly unsafe Vector256<int> RotateRight = Vector256.Create(7, 0, 1, 2, 3, 4, 5, 6);
 
 
         /* [Benchmark] unsafe public int[,] IntegrateUnsafe2Pass() // 4 place; +114% to the winner
