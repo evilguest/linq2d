c66a1f390edd6ed26f09f1928b55162680715bda
Anton Zlygostev <zlygostev@hotmail.com>
2023-12-28 22:53:07 +0700

    Sse41 100% coverage

--- Linq2d.Tests.Vectorization/Sse41.cs
+++ Linq2d.Tests.Vectorization/Sse41.cs
@@ -1,8 +1,4 @@
-﻿using Linq2d.CodeGen.Intrinsics;
-using System;
-using Xunit;
-
-namespace Linq2d.Tests.Vectorization
+﻿namespace Linq2d.Tests.Vectorization
 {
     public class Sse41 : Base, IClassFixture<SuppressAvxFixture>
     {
@@ -16,11 +12,110 @@ namespace Linq2d.Tests.Vectorization
             var iv = (IVectorizable)q;
             AssertVectorized(iv, 4);
         }
-        private static double[,] Sqrt(int[,] source)
+
+        [Fact]
+        public void UIntMul()
         {
-            return (from s in source select Math.Sqrt(s)).ToArray();
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42, x => (uint)x);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 17, x => (uint)x);
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, 17, (x, y) => (uint)(x*y));
+            var q = from s1 in source1
+                    from s2 in source2
+                    select s1 * s2;
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
         }
 
+        [Fact]
+        public void IntMax()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 17);
+
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, 17, Math.Max);
+            var q = from s1 in source1
+                    from s2 in source2
+                    select Math.Max(s1, s2);
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void IntMin()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 17);
+
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, 17, Math.Min);
+            var q = from s1 in source1
+                    from s2 in source2
+                    select Math.Min(s1, s2);
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void UIntMax()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42, x => (uint)x);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 17, x => (uint)x);
+
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, 17, (x, y)=>Math.Max((uint)x,(uint)y));
+            var q = from s1 in source1
+                    from s2 in source2
+                    select Math.Max(s1, s2);
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void UIntMin()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42, x => (uint)x);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 17, x => (uint)x);
+
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, 17, (x, y) => Math.Min((uint)x, (uint)y));
+            var q = from s1 in source1
+                    from s2 in source2
+                    select Math.Min(s1, s2);
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        //private static double[,] Sqrt(int[,] source)
+        //{
+        //    return (from s in source select Math.Sqrt(s)).ToArray();
+        //}
+
+
+        [Fact] 
+        public void ShortConditional()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42, x=>(short)x);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 42, x => x > 0 ? (short)x : (short)-x);
+            var expect = ArrayHelper.InitAllRand(100, 110, 42, x => x >= 0 ? (short)11 : (short)-17);
+            var q = from s1 in source1
+                    from s2 in source2
+                    select s1 == s2 ? (short)11 : (short)-17;
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 8);
+        }
+        [Fact]
+        public void UShortConditional()
+        {
+            var source1 = ArrayHelper.InitAllRand(100, 110, 42, x => (ushort)x);
+            var source2 = ArrayHelper.InitAllRand(100, 110, 42, x => (ushort)(x & 0xFFFE));
+            var expect = ArrayHelper.InitAllRand(100, 110, 42,  x =>  x%2==0? (ushort)11 : (ushort)17);
+            var q = from s1 in source1
+                    from s2 in source2
+                    select s1 == s2 ? (ushort)11 : (ushort)17;
+            Assert.Equal(expect, q.ToArray());
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 8);
+        }
+        
         [Fact]
         public void TestByteCopyOptimization()
         {
@@ -42,6 +137,127 @@ namespace Linq2d.Tests.Vectorization
 
             var iv = (IVectorizable)q;
             AssertVectorized(iv, 16);
+        }
+
+        [Fact] 
+        public void TestByteToIntConversion() 
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (byte)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => x & 0xFF);
+            var q = from s in source select (int)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void TestSByteToIntConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (sbyte)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (int)(sbyte)x);
+            var q = from s in source select (int)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void TestShortToIntConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (short)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (int)(short)x);
+            var q = from s in source select (int)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void TestUShortToIntConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (ushort)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (int)(ushort)x);
+            var q = from s in source select (int)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void TestByteToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42L, x => (byte)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42L, x => x & 0xFF);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
+        }
+        [Fact]
+        public void TestSByteToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (sbyte)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (long)(sbyte)x);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
+        }
+        [Fact]
+        public void TestShortToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (short)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (long)(short)x);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
+        }
+        [Fact]
+        public void TestUShortToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (ushort)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (long)(ushort)x);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
+        }
+        [Fact]
+        public void TestIntToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (long)x);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
+        }
+        [Fact]
+        public void TestUIntToLongConversion()
+        {
+            var source = ArrayHelper.InitAllRand(17, 17, 42, x => (uint)x);
+            var expect = ArrayHelper.InitAllRand(17, 17, 42, x => (long)(uint)x);
+            var q = from s in source select (long)s;
+
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 2);
         }
         [Fact]
         public void TestIntComparison128()
@@ -95,6 +311,9 @@ namespace Linq2d.Tests.Vectorization
         public void TestFloatConditional128() => TestConditionalInt128((float)Math.PI, (float)Math.E, sizeof(float));
         [Fact]
         public void TestLongConditional128() => TestConditionalLong128<long>(7, -42, sizeof(long));
+
+        [Fact]
+        public void TestULongConditional128() => TestConditionalLong128<ulong>(7, 42, sizeof(ulong));
 
     }
 }
