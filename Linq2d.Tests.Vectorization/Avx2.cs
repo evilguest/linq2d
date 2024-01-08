4753afde56a4c2e03ff9e434227af68e57768889
Anton Zlygostev <zlygostev@hotmail.com>
2024-01-08 16:32:06 +0700

    Improving Avx2 coverage

--- Linq2d.Tests.Vectorization/Avx2.cs
+++ Linq2d.Tests.Vectorization/Avx2.cs
@@ -518,5 +518,85 @@ namespace Linq2d.Tests.Vectorization
             AssertVectorized(iv, 32);
         }
 
+        [Fact]
+        public void IntMixedArith()
+        {
+            var source1 = ArrayHelper.InitAllRand(70, 40, 42, x => (byte)x);
+            var source2 = ArrayHelper.InitAllRand(70, 40, 17, x => (sbyte)x);
+            var source3 = ArrayHelper.InitAllRand(70, 40, 42, x => (short)x);
+            var source4 = ArrayHelper.InitAllRand(70, 40, 17, x => (ushort)x);
+            var source0 = ArrayHelper.InitAllRand(70, 40, 42);
+            var expect = ArrayHelper.InitAllRand(70, 40, 42, 17, (x, y) => (byte)x + (sbyte)y + (short)x + (ushort)y + x);
+            var q = from s in source0
+                    from s1 in source1
+                    from s2 in source2
+                    from s3 in source3
+                    from s4 in source4
+                    select s1 + s2 + s3 + s4 + s;
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 8);
+        }
+        [Fact]
+        public void LongMixedArith()
+        {
+            var source1 = ArrayHelper.InitAllRand(70, 40, 42L, x => (byte)x);
+            var source2 = ArrayHelper.InitAllRand(70, 40, 17L, x => (sbyte)x);
+            var source3 = ArrayHelper.InitAllRand(70, 40, 42L, x => (short)x);
+            var source4 = ArrayHelper.InitAllRand(70, 40, 17L, x => (ushort)x);
+            var source5 = ArrayHelper.InitAllRand(70, 40, 42L, x => (int)x);
+            var source6 = ArrayHelper.InitAllRand(70, 40, 17L, x => (uint)x);
+            var source0 = ArrayHelper.InitAllRand(70, 40, 42L);
+            var expect = ArrayHelper.InitAllRand(70, 40, 42L, 17L, (x, y) => (byte)x + (sbyte)y + (short)x + (ushort)y + (int)x + (uint)y + x);
+            var q = from s in source0
+                    from s1 in source1
+                    from s2 in source2
+                    from s3 in source3
+                    from s4 in source4
+                    from s5 in source5
+                    from s6 in source6
+                    select s1 + s2 + s3 + s4 + s5 + s6 + s;
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 4);
+        }
+        [Fact]
+        public void IntToShortConversion()
+        {
+            var source = ArrayHelper.InitAllRand(70, 40, 42);
+            var expect = ArrayHelper.InitAllRand(70, 40, 42, x=>(short)x);
+            var q = from s in source
+                    select (short)s;
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 8);
+        }
+        [Fact]
+        public void ShortByteArith()
+        {
+            var source = ArrayHelper.InitAllRand(70, 40, 42, x=>(byte)x);
+            var expect = ArrayHelper.InitAllRand(70, 40, 42, x => (short)(byte)x);
+            var q = from s in source
+                    select (short)s;
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 16);
+        }
+        [Fact]
+        public void ShortSByteArith()
+        {
+            var source = ArrayHelper.InitAllRand(70, 40, 42, x => (sbyte)x);
+            var expect = ArrayHelper.InitAllRand(70, 40, 42, x => (short)(sbyte)x);
+            var q = from s in source
+                    select (short)s;
+            Assert.Equal(expect, q.ToArray());
+
+            var iv = (IVectorizable)q;
+            AssertVectorized(iv, 16);
+        }
     }
 }
